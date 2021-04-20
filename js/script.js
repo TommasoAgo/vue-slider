961ac9433829a5b99f030b9cd03e8a0992c67ef9
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-20 15:59:43 +0200

    Riuscito a risolvere con la classe active che si buggava

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@ var app = new Vue(
     {
         el : '#root',
         data : {
-            
+            activeImage : 0,
+            images : [
+                'img/campagna.jpg',
+                'img/cascate.webp',
+                'img/fiori.jpeg',
+                'img/montagne.webp',
+            ]
+        },
+        methods : {
+           
         }
     }
 );
