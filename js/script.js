e0310ab659c17cbf177f98b3b633f7f85ba7d086
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-20 16:39:02 +0200

    Aggiunto commenti al JavaScript

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var app = new Vue(
     {
         el : '#root',
         data : {
+            // Setto la prima immagine da stampare come primo elemento dell'Array
             activeImage : 0,
+            // Creo un Array di immagini
             images : [
                 'img/campagna.jpg',
                 'img/cascate.webp',
@@ -11,6 +13,7 @@ var app = new Vue(
             ]
         },
         methods : {
+            // Funzione che andrà ad attivarsi all'evento click su Right Arrow
            nextImage() {
                if ( this.activeImage == this.images.length - 1 ) {
                    this.activeImage = 0;
@@ -18,6 +21,7 @@ var app = new Vue(
                    this.activeImage = this.activeImage + 1;
                }
            },
+            // Funzione che andrà ad attivarsi all'evento click su Right Arrow
            prevImage() {
                if ( this.activeImage == 0 ) {
                    this.activeImage = this.images.length - 1;
@@ -26,6 +30,7 @@ var app = new Vue(
                }
            }
         },
+        // Funzione che fa partire lo Slide Show ogni 3 secondi
         created() {
             setInterval(() => {
                 this.nextImage()
